e3939d5cc967cae648aef8900de63e326a3b6404
airesdev <aires.gabee@gmail.com>
2024-05-18 23:50:45 -0300

    updated

--- html/projects/book_library/script.js
+++ html/projects/book_library/script.js
@@ -1,23 +1,42 @@
-const input = document.querySelector("#user")
-const div = document.querySelector("#divTarefas")
-const form = document.querySelector("#forms")
+const addBookForm = document.querySelector("#addBookForm")
 
+addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-function add(event) {
-    event.preventDefault()
-    // prevenir de reiniciar página
-    console.log(event)
-    // mostrar event
-    const newItem = document.createElement("p")
-    // criar elemento
-    newItem.textContent = input.value
-    // elemento tera o conteúdo do input
-    div.appendChild(newItem)
-    // adicionando à div
-    input.value = ""
-    // esvasiando o input
-    input.focus() 
-    // focando no input
+    const title = document.querySelector("#title").value
+    const author = document.querySelector("#author").value
+    const cover = document.querySelector("#cover").value
+    const genre = document.querySelector("#genre").value
+    const year = document.querySelector("#year").value
+
+    addBook(title, author, cover, genre, year)
+});
+
+const list = []
+const book = {
+    title: "title",
+    author: "author",
+    cover: "cover",
+    genre: "genre",
+    year: "year",
+    stars: 0
 }
 
-form.addEventListener("submit", add)
+function addBook (title, author, cover, genre, year) {
+    const newBook = Object.create(book)
+    newBook.title = title
+    newBook.author = author
+    newBook.cover = cover
+    newBook.genre = genre
+    newBook.year = year
+    list.push(newBook)
+    console.log(list)
+}
+
+function search (search) {
+
+}
+
+function comment (comment, stars) {
+
+}
--- html/projects/projeto_netflix/script.js
+++ html/projects/projeto_netflix/script.js
@@ -1,14 +1,79 @@
-async function buscarCep(){
+async function get(list, key, lang){
     try{
-        const requisicao = await fetch("")
+        const requisicao = await fetch("https://api.themoviedb.org/3/movie/" + list + "?api_key=" + key + "&language=" + lang)
         const resposta = await requisicao.json()
-        console.log(resposta)
-
-        // específico da api
-        if(resposta.erro){
-            console.log("CEP inválido")
-        }
+        return resposta
     }catch(error){
-        console.log("Error na API")
+        console.log("Error ao acessar a API")
     }
+}
+
+function adicionar(id, type) {
+
+    // título
+    const titulo = document.createElement("h2")
+    titulo.textContent = id.title
+    titulo.classList.add("titulo")
+
+    // imagem
+    const img = document.createElement("img")
+    img.src = "https://image.tmdb.org/t/p/w500" + id.poster_path
+    img.classList.add("poster")
+
+    // rating
+    const rating = document.createElement("img")
+    rating.classList.add("rating")
+
+    if (id.adult = true) {rating.src = "/content/adulto.png"}
+    else {rating.src = "/content/livre.png"}
+
+    // votos
+    const votos = document.createElement("div")
+    votos.classList.add("votos")
+
+    const estrela = document.createElement("img")
+    estrela.src = "/content/star.png"
+    votos.append(estrela)
+    const average = document.createElement("p")
+    average.textContent = id.vote_average
+    votos.append(average)
+
+    // year
+    const year = document.createElement("p")
+    year.textContent = id.release_date
+    year.classList.add("year")
+
+    // const reminder = document.createElement("button")
+    // const more = document.createElement("button")
+
+    // poster
+    const poster = document.createElement("div")
+    // poster.classList.add("filme")
+
+    if (type = "vertical") {
+        poster.classList.add("vertical")
+    }else if (type = "horizontal") {
+        poster.classList.add("horizontal")
+    }else if (type = "upcoming") {
+        poster.classList.add("upcoming")
+    }else if (type = "top") {
+        poster.classList.add("top")
+    }else {
+        console.log("Erro no tipo de poster")
+    }
+
+    // append
+    poster.appendChild(titulo)
+    poster.appendChild(img)
+    poster.appendChild(rating)
+    poster.appendChild(votos)
+    poster.appendChild(year)
+
+    document.appendChild(poster)
+}
+
+const popular = get("popular", "77c4e2b070a2e1396500d0b42ebf7cec", "pt-BR").results
+
+for (let id of popular) {
+    adicionar(id, "vertical")
 }
